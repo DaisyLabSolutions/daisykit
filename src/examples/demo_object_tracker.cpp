3c2474b507800ac2f5655c0cddf144a464b93221
phalc <v.phalc@vinai.io>
2021-09-26 00:35:54 +0700

    Create the structure of files to reproduce an example

---
+++ src/examples/demo_object_tracker.cpp
@@ -0,0 +1,38 @@
+// Copyright 2021 The DaisyKit Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "daisykitsdk/common/types.h"
+#include "daisykitsdk/flows/object_detector_flow.h"
+#include "daisykitsdk/flows/object_tracker_flow.h"
+#include "third_party/json.hpp"
+
+#include <stdio.h>
+#include <fstream>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <streambuf>
+#include <string>
+
+using namespace cv;
+using namespace std;
+using json = nlohmann::json;
+using namespace daisykit;
+using namespace daisykit::flows;
+
+int main(int argc, char* argv[])
+{
+    // import flow
+        // config_str
+    // start what the demo does
+}
